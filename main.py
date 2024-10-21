c052bd256a6f03b53cdba6019609039b855a39f8
PaletteJack <barbozalouie@gmail.com>
2024-10-21 10:51:57 -0700

    added static folder. removed axios and modified csv load script

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_file
 import sqlite3
 import pandas as pd
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.jinja_env.auto_reload = True
+
+@app.route('/export', methods=["POST"])
+def export_csv():
+    query = request.json["query"]
+    conn = sqlite3.connect('db.sqlite')
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    csv_file = df.to_csv('query_export.csv', index=False)
+    return send_file(csv_file, as_attachment=True, attachment_filename='query_export.csv', mimetype='text/csv')
 
 @app.route('/query', methods=["POST"])
 def run_query():
     query = request.json["query"]
-    conn = sqlite3.connect('school_db.sqlite')
+    conn = sqlite3.connect('db.sqlite')
     df = pd.read_sql_query(query, conn)
     conn.close()
     return jsonify(df.to_dict(orient='records'))
@@ -16,5 +27,5 @@ def run_query():
 def home():
     return render_template('index.html', title="Home")
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(debug=True)
--- scripts/load_db.py
+++ scripts/load_db.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import sqlite3
+import pathlib
+ROOT_DIR = pathlib.Path(__file__).parent.resolve()
+
+def snake_case(word):
+    return word.strip().lower().replace(" ", "_")
 
 def seed_data(csv_file, db_file, table_name):
     df = pd.read_csv(csv_file)
+    df.columns = [snake_case(col) for col in df.columns]
+    print("New column names:")
+    print(df.columns.tolist())
     conn = sqlite3.connect(db_file)
     df.to_sql(table_name, conn, if_exists='replace', index=False)
     conn.close()
     
 def main():
     try:
-        seed_data("school_data.csv", "school_db.sqlite", "school_data")
+        seed_data("../csvs/school_data.csv", "../db.sqlite", "school_data")
         print("loaded csv to database")
     except Exception as e:
         print(e)
