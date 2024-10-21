66dd39e85f2aaceee76d56e2723f37a87f10cc35
PaletteJack <barbozalouie@gmail.com>
2024-10-21 11:52:32 -0700

    modified load_data script to correctly convert headers to snake_case

--- scripts/load_db.py
+++ scripts/load_db.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import sqlite3
-import pathlib
-ROOT_DIR = pathlib.Path(__file__).parent.resolve()
+import re
 
 def snake_case(word):
-    return word.strip().lower().replace(" ", "_")
-
+    base_word = re.sub(r'[^a-zA-Z0-9\s]', '', word.strip())
+    words = base_word.lower().split()
+    return '_'.join(words)
+    
 def seed_data(csv_file, db_file, table_name):
     df = pd.read_csv(csv_file)
     df.columns = [snake_case(col) for col in df.columns]
-    print("New column names:")
-    print(df.columns.tolist())
     conn = sqlite3.connect(db_file)
     df.to_sql(table_name, conn, if_exists='replace', index=False)
     conn.close()
+    print("database created")
     
 def main():
     try:
